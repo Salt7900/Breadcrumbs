7f0f53ffee0ea91b69c8df8a117b7f309435aab4
Salt7900 <bnfallon@gmail.com>
2015-11-17 16:49:00 -0600

    Initial Commit

---
+++ breadCrumbs/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  breadCrumbs
+//
+//  Created by Ben Fallon on 11/17/15.
+//  Copyright © 2015 Ben Fallon. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
