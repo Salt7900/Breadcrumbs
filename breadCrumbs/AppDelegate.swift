bb68564c294a0800c17bfc6a00ba92237f5262e9
Salt7900 <bnfallon@gmail.com>
2015-11-21 15:24:01 -0600

    add fixed files

---
+++ breadCrumbs/AppDelegate.swift
@@ -0,0 +1,89 @@
+//
+//  AppDelegate.swift
+//  breadCrumbs
+//
+//  Created by Ben Fallon on 11/17/15.
+//  Copyright © 2015 Ben Fallon, Jen Trudell, and Katelyn Dinkgrave. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate{
+var window: UIWindow?
+
+let locationManager = CLLocationManager()
+
+    let userSession = Main()
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+
+        if(UIApplication.instancesRespondToSelector(Selector("registerUserNotificationSettings:"))) {
+            UIApplication.sharedApplication().registerUserNotificationSettings(UIUserNotificationSettings(forTypes: [.Alert, .Sound, .Badge], categories: nil))
+        }
+        UIApplication.sharedApplication().cancelAllLocalNotifications()
+
+
+        return true
+    }
+
+    func applicationWillResignActive(application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(application: UIApplication) {
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+    //BEN - Allow notifications when a geofence has been crossed
+    func handleRegionEvent(region: CLRegion!){
+        if UIApplication.sharedApplication().applicationState == .Active {
+            if let message = notefromRegionIdentifier(region.identifier) {
+                if let viewController = window?.rootViewController {
+                    showSimpleAlertWithTitle(nil, message: message, viewController: viewController)
+                    print("HELLO FROM LOCAL")
+                }
+            }
+        } else {
+            // Otherwise present a local notification
+            var notification = UILocalNotification()
+            notification.alertBody = "Region Crossed"
+            notification.soundName = "Default";
+            print("HELLO FROM GLOBAL")
+            UIApplication.sharedApplication().presentLocalNotificationNow(notification)
+        }
+    }
+
+    func locationManager(manager: CLLocationManager, didEnterRegion region: CLRegion) {
+            handleRegionEvent(region)
+    }
+
+    func notefromRegionIdentifier(identifier: String) -> String? {
+            for savedItem in Main().returnCrumb() {
+                    if savedItem.identity == identifier {
+                        return savedItem.title
+                    }
+            }
+        return "You have a notification"
+    }
+
+
+}
