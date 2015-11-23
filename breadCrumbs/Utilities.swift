ffc42f4a75f6c60d109af7b41a20f69c81ab02d4
Salt7900 <bnfallon@gmail.com>
2015-11-23 09:01:48 -0600

    add retrieve photo method

--- breadCrumbs/Utilities.swift
+++ breadCrumbs/Utilities.swift
@@ -17,3 +17,22 @@ func showSimpleAlertWithTitle(title: String!, message: String, viewController: U
     alert.addAction(action)
     viewController.presentViewController(alert, animated: true, completion: nil)
 }
+
+
+//Used to fetch image Asynchronously BEN
+//func downloadImage(url: NSURL){
+//    print("Started downloading \"\(url.URLByDeletingPathExtension!.lastPathComponent!)\".")
+//    getDataFromUrl(url) { (data, response, error)  in
+//        dispatch_async(dispatch_get_main_queue()) { () -> Void in
+//            guard let data = data where error == nil else { return }
+//            print("Finished downloading \"\(url.URLByDeletingPathExtension!.lastPathComponent!)\".")
+//            imageURL.image = UIImage(data: data)
+//        }
+//    }
+//}
+//
+//func getDataFromUrl(url:NSURL, completion: ((data: NSData?, response: NSURLResponse?, error: NSError? ) -> Void)) {
+//    NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) in
+//        completion(data: data, response: response, error: error)
+//        }.resume()
+//}
