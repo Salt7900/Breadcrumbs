419a9da3c5d12e3d3808ba687fcfe904f2c40bf4
Jen Trudell <jtrudell@gmail.com>
2015-11-23 18:10:52 -0600

    add route back to login after logout

--- breadCrumbs/SettingsViewController.swift
+++ breadCrumbs/SettingsViewController.swift
@@ -3,11 +3,36 @@
 //  breadCrumbs
 //
 //  Created by Ben Fallon on 11/23/15.
-//  Copyright © 2015 Ben Fallon. All rights reserved.
+//  Copyright © 2015 Ben Fallon, Katelyn Dinkgrave and Jen Trudell. All rights reserved.
 //
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+let userDefaults = NSUserDefaults.standardUserDefaults()
 
+class SettingsViewController: UIViewController {
+    
+//JEN label top of view with email of user
+    
+    @IBOutlet weak var userEmailLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //set label to user email
+        let userEmail = userDefaults.objectForKey("email") as! String
+        userEmailLabel.text = userEmail
+    }
+    
+// JEN attach logout button
+
+    @IBAction func logoutPressed(sender: AnyObject) {
+        CrumbUser.logOut()
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let loginVC = storyboard.instantiateViewControllerWithIdentifier("LoginVC") as! LoginViewController
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        appDelegate.window?.rootViewController = loginVC
+
+    }
 }
